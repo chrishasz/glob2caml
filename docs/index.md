7ad1c0e6d5c5e1de85719dfeccd6e9657f028a53
chrishasz <chrishasz@gmail.com>
2019-05-14 15:07:23 -0700

    initial documentation commit

---
+++ docs/index.md
@@ -0,0 +1,10 @@
+---
+layout: project
+title: Home
+---
+
+# Glob 2 Caml
+
+Glob2Caml is a simple Typescript library which generates a CAML query from a Glob String.
+
+[Getting Started with Glob2Caml](/glob2caml/general/getting-started)
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+'use strict';
+/**
+* @Method: Returns the plural form of any noun.
+* @Param {string}
+* @Return {string}
+*/
+export function Convert(glob : any, spSitePath : string) : string {
+    let camlQuery : string = '';
+    
+    return camlQuery;
+}
